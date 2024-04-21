4f669d161552fca89999bd2b702c5a3b6c01fed0
VijayNapworks <105584645+ThakurVijay2191@users.noreply.github.com>
2024-04-21 17:29:28 +0530

    bugs fixed

--- Sources/MyLibrary/ConditionalNavigation.swift
+++ Sources/MyLibrary/ConditionalNavigation.swift
@@ -8,17 +8,17 @@
 import SwiftUI
 
 @available(iOS 13.0, *)
-struct ConditionalNavigation<Destination: View>: View {
+public struct ConditionalNavigation<Destination: View>: View {
     @ViewBuilder var destination: Destination
     @Binding var isActive: Bool
     var isNavigationBarHidden: Bool
     
-    init(@ViewBuilder destination: @escaping ()-> Destination, isActive: Binding<Bool>, isNavigationBarHidden: Bool = false) {
+    public init(@ViewBuilder destination: @escaping ()-> Destination, isActive: Binding<Bool>, isNavigationBarHidden: Bool = false) {
         self.destination = destination()
         self._isActive = isActive
         self.isNavigationBarHidden = isNavigationBarHidden
     }
-    var body: some View {
+    public var body: some View {
         NavigationLink("", destination: destination.navigationBarBackButtonHidden(isNavigationBarHidden), isActive: $isActive)
     }
 }
